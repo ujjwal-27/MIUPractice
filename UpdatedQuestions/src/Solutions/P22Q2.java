7d5eb710a85a59324c6f05a89247e313dcfd564f
ujjwal-27 <ujjwalshresth.96@gmail.com>
2024-12-31 07:51:31 +0545

    initial commit for isPacked

---
+++ UpdatedQuestions/src/Solutions/P22Q2.java
@@ -0,0 +1,16 @@
+package Solutions;
+
+public class P22Q2 {
+    public static void main(String[] args) {
+        P22Q2 myClass = new P22Q2();
+
+        int[] a = {2, 2, 3, 3, 3};
+
+        int result = myClass.isPacked(a);
+        System.out.println(result);
+    }
+
+    public int isPacked(int[] a) {
+
+    }
+}

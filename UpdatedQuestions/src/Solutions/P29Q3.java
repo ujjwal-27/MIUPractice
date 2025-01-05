8c5f11b1e95dd79b45d51f1bb174c2664608cc27
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-01-05 08:49:43 +0545

    implement matchPattern method

--- UpdatedQuestions/src/Solutions/P29Q3.java
+++ UpdatedQuestions/src/Solutions/P29Q3.java
@@ -2,14 +2,21 @@ package Solutions;
 
 public class P29Q3 {
     public static void main(String[] args) {
+//        Edge cases:
+//        int[] a = {1, 1, 1, 2, 2, 1, 1};
+//        int[] p = {1, 2, 1, 3};
+//
+//        int[] a = {1, 1, 1, 2, 2, 1, 1, 3};
+//        int[] p = {1, 2, 1};
+
         int[] a = {1, 1, 1, 2, 2, 1, 1, 3};
-        int[] p = {1, 2, 1, 3};
+        int[] p = {1, 2, 1};
 
         int result = matchPattern(a, p);
         System.out.println(result);
     }
 
-    static int matchPattern(int[] a, int[] pattern) {
+    public static int matchPattern(int[] a, int[] pattern) {
         // len is the number of elements in the array 'a', patternLen is the number of elements in the pattern.
         int i = 0; // index into a
         int k = 0; // index into pattern
@@ -24,16 +31,19 @@ public class P29Q3 {
             } else if (matches == 0 || k == patternLen - 1) {
                 return 0; // if pattern[k] was never matched (matches==0) or at end of pattern (k==patternLen-1)
 
-            } else // advance to next pattern character {
-                !!You write this code!!
+            } else { // advance to next pattern character
+                i--;
+                k++;
+                matches = 0;
             }
         }
 
         // return 1 if at end of array a (i==len) and also at end of pattern (k==patternLen-1)
-        if (i == len && k == patternLen-1){
+        if (i == len && k == patternLen - 1) {
             return 1;
 
         } else {
             return 0;
         }
+    }
 }

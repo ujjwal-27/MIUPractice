fa4bf400128ccc1be182e384cfebc3e40585edce
ujjwal-27 <ujjwalshresth.96@gmail.com>
2025-03-04 12:05:32 +0545

    initial commit for systematically increasing

---
+++ UpdatedQuestions/src/Solutions/P36Q1.java
@@ -0,0 +1,13 @@
+package Solutions;
+
+public class P36Q1 {
+    public static void main(String[] args) {
+        int[] a = {1, 1, 2, 1, 2, 3, 1, 2, 3, 4};
+        int result = isSystematicallyIncreasing(a);
+        System.out.println(result);
+    }
+
+    public static int isSystematicallyIncreasing(int[] a) {
+        
+    }
+}
